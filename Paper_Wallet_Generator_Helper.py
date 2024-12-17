37bffcc56c3a6087b70c1d54be5fd5d21ce38bd4
Fleischkuechle <zimmkon@yahoo.de>
2024-12-17 23:46:42 +0100

    update on app layout dded print , generate and save as pdf button

--- Paper_Wallet_Generator_Helper.py
+++ Paper_Wallet_Generator_Helper.py
@@ -1,5 +1,12 @@
-from PIL import Image as pil_img
+import os
 import qrcode
+from PIL import Image as pil_img
+import fitz  # PyMuPDF
+
+from pdf_utils.PDF_Helper import PDF_Helper
+from coin_utils.coin_utils.Create_Address_Doge import Create_Address_Doge
+from pdf_utils.Draw_Wallet import Draw_Wallet
+from pdf_utils.ImagePrinter import ImagePrinter
 
 
 class Paper_Wallet_Generator_Helper:
@@ -7,6 +14,88 @@ class Paper_Wallet_Generator_Helper:
         self.width: int = 800
         self.height: int = self.width
         self.img_size: tuple[int, int] = (self.width, self.height)
+
+        self.pdf_helper: PDF_Helper = PDF_Helper()
+        self.create_address_doge: Create_Address_Doge = Create_Address_Doge()
+        self.drawwallet: Draw_Wallet = Draw_Wallet()
+        self.image_printer: ImagePrinter = ImagePrinter()
+        self.privkey: str = ""
+        self.pub_address: str = ""
+        self.privkey_qr_img: pil_img.Image = None
+        self.pub_address_qr_img: pil_img.Image = None
+        self.pdf_as_bytes: bytes = bytes()
+        self.pdf_document: fitz.Document = None
+        self.pix_map: fitz.Pixmap = None
+        self.Generate_new_paper_wallet()
+
+    def Generate_new_paper_wallet(
+        self,
+    ) -> fitz.Document:
+
+        self.privkey, self.pub_address = self.create_address_doge.make_address_doge(
+            print_result=False
+        )
+
+        self.privkey_qr_img = self.generate_qrcode_img(qr_text=self.privkey)
+        self.pub_address_qr_img: pil_img.Image = self.generate_qrcode_img(
+            qr_text=self.pub_address
+        )
+        self.pdf_as_bytes = self.drawwallet.draw_wallet_return_bytes_2(
+            pub_address=self.pub_address,
+            pub_address_qr_img=self.pub_address_qr_img,
+            privkey=self.privkey,
+            privkey_qr_img=self.privkey_qr_img,
+        )
+
+        self.pdf_document = self.pdf_helper.open_pdf_from_bytes(
+            pdf_bytes=self.pdf_as_bytes
+        )
+        self.pix_map = self.pdf_helper.get_current_pixmap()
+        return self.pdf_document
+
+    def save_paralell(
+        self,
+    ) -> tuple[bool, str]:
+        is_saved: bool = False
+        path: str = ""
+        folder_path: str = os.path.dirname(__file__)
+        path = os.path.join(folder_path, self.pub_address + ".pdf")
+        # self.pdf_document.save_snapshot(filename=path)
+        self.pdf_document.save(filename=path)
+        is_saved = True
+        return is_saved, path
+
+    def save_as_pdf(self, folder_path: str = "") -> tuple[bool, str]:
+        is_saved: bool = False
+        path: str = ""
+        if folder_path != "":
+            # directory_path:str = os.path.dirname(folder_path)
+            if os.path.exists(path=folder_path):
+                path = os.path.join(folder_path, self.pub_address + ".pdf")
+                # self.pdf_document.save_snapshot(filename=path)
+                self.pdf_document.save(filename=path)
+                is_saved = True
+                return is_saved, path
+            else:
+                is_saved, path = self.save_paralell()
+        else:
+            is_saved, path = self.save_paralell()
+
+        return is_saved, path
+
+    def print_paper_wallet(
+        self,
+    ):
+        self.image_printer.print_pdf_from_memory(
+            pdf_data=self.pdf_as_bytes,
+            document_name=self.pub_address,
+        )
+
+    def get_current_pixmap(
+        self,
+    ):
+        # self.pix_map = self.pdf_helper.get_current_pixmap()
+        return self.pix_map
 
     def generate_qrcode_img(
         self,
